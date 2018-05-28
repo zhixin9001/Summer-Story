003cc5197cbd052165256298b9c69f0421dcfb36
zhixin9001 <zhixin9001@live.com>
2018-05-29 06:58:24 +0800

    detail page

---
+++ MiniProgram/pages/detail/detail.js
@@ -0,0 +1,63 @@
+// pages/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    photos: [
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2380677316.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2386736909.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2382076389.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2388681695.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2387538436.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2380677316.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2380677316.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2380677316.jpg" },
+      { src: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2380677316.jpg" },
+    ],
+    content: '',
+    remain: {
+      num: 540,
+      hidden: true
+    },
+    location: '位置',
+    x: 0,
+    y: 0
+  },
+  changeTips: function (e) {
+    let value = e.detail.value;
+    let remain = this.data.remain;
+
+    if (remain.hidden) {
+      remain.hidden = false;
+    };
+    remain.num = 540 - value.length;
+
+    this.setData({
+      remain: remain,
+      content: value
+    });
+  },
+  chooseLocation: function () {
+    let _location = this.data.location;
+    let _x = this.data.x;
+    let _y = this.data.y;
+    let _this=this;
+    wx.chooseLocation({
+      success: function (res) {
+        _location = res.name;
+        _x = res.latitude;
+        _y = res.longitude
+
+        _this.setData({
+          location: _location,
+          x: _x,
+          y: _y
+        });
+      }
+    })
+  },
+
+
+})
