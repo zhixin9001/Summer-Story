99d69b756695c2098738615d1ccef8fcccf2f646
zhixin9001 <zhixin9001@live.com>
2018-06-05 07:31:00 +0800

    init

---
+++ API/SummerStoryService/SummerStoryService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SummerStoryService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
