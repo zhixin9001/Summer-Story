4caa3be2ba312136ee3626a80b032bc2b060399c
dz33 <zhixin9001@live.com>
2018-06-09 12:38:56 +0800

    create table

--- API/SummerStoryService/Common/SaveImgInCloud.cs
+++ API/SummerStoryService/Common/SaveImgInCloud.cs
@@ -13,7 +13,7 @@ public class SaveImgInCloud
         // 上传文件名
         string key = "key" + DateTime.Now.ToString();
         // 本地文件路径
-        string filePath = "E:\\Capture.JPG";
+        string filePath = "D:\\Untitled.png";
         // 存储空间名
         string Bucket = "summerystory";
         // 设置上传策略，详见：https://developer.qiniu.com/kodo/manual/1206/put-policy
@@ -33,6 +33,7 @@ public class SaveImgInCloud
         // 表单上传
         FormUploader target = new FormUploader(config);
         HttpResult result = target.UploadFile(filePath, key, token, null);
+        
         return result.ToString();
 
     }
---
+++ API/SummerStoryService/CreateTable.sql
@@ -0,0 +1,28 @@
+﻿--User
+CREATE TABLE Summer_User(
+	ID
+	WxID
+	CreatedDatetime
+)
+
+--Record
+CREATE TABLE Summer_Record(
+	ID
+	UserID
+	CreatedDatetime
+)
+
+--Text
+CREATE TABLE Summer_Text(
+	ID
+	RecordID
+	Content
+)
+
+--Image
+CREATE TABLE Summer_Image(
+	ID
+	RecordID
+	ImageName
+	Cloud
+)
